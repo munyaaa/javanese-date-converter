7bc0b75a2b9829841b5bbed41110360b485535e0
Jessica Jimantoro <munyaaa@Jessicas-MacBook-Air.local>
2021-04-16 15:06:07 +0700

    feat: add associated symbols test: add associated symbols test

--- lib/javanese_date/pasaran.dart
+++ lib/javanese_date/pasaran.dart
@@ -12,6 +12,8 @@ enum PasaranType {
 abstract class IPasaran {
   PasaranType get pasaranType;
   String get pasaran;
+  /// Pasaran symbols in general
+  Map<String, String> get associatedSymbols;
 }
 
 class Pasaran implements IPasaran {
@@ -41,7 +43,12 @@ class Pasaran implements IPasaran {
 
   int _calculatePasaran(JavaneseDate date) {
     final FormulaInit fx = FormulaInit(date);
-    return (fx.k + ((3 * (fx.m + 1)) ~/ 5) + (fx.y ~/ 4) + (4 * fx.c) - (4 * (fx.c ~/ 4))) % 5;
+    return (fx.k +
+            ((3 * (fx.m + 1)) ~/ 5) +
+            (fx.y ~/ 4) +
+            (4 * fx.c) -
+            (4 * (fx.c ~/ 4))) %
+        5;
   }
 
   @override
@@ -61,5 +68,93 @@ class Pasaran implements IPasaran {
         throw Exception("Error: Incorrect pasaran");
         break;
     }
+  }
+
+  @override
+  Map<String, String> get associatedSymbols {
+    switch (this.pasaranType) {
+      case PasaranType.PON:
+        return AssociatedSymbols(
+          krama: "Petak",
+          meaning: "",
+          element: "water",
+          color: "yellow",
+          direction: "west",
+          posture: "sleep",
+        ).toJson();
+      case PasaranType.WAGE:
+        return AssociatedSymbols(
+          krama: "Cemeng",
+          meaning: "dark",
+          element: "earth",
+          color: "black",
+          direction: "north",
+          posture: "sit down",
+        ).toJson();
+      case PasaranType.KLIWON:
+        return AssociatedSymbols(
+          krama: "Asih",
+          meaning: "affection",
+          element: "spirit",
+          color: "mixed color",
+          direction: "center",
+          posture: "stand up",
+        ).toJson();
+      case PasaranType.LEGI:
+        return AssociatedSymbols(
+          krama: "Manis",
+          meaning: "sweet",
+          element: "air",
+          color: "white",
+          direction: "east",
+          posture: "turn back",
+        ).toJson();
+      case PasaranType.PAHING:
+        return AssociatedSymbols(
+          krama: "Pahit",
+          meaning: "bitter",
+          element: "fire",
+          color: "red",
+          direction: "south",
+          posture: "to face",
+        ).toJson();
+      default:
+        throw Exception("Error: Incorrect pasaran");
+        break;
+    }
+  }
+}
+
+abstract class IAssociatedSymbols {
+  Map<String, dynamic> toJson();
+}
+
+class AssociatedSymbols implements IAssociatedSymbols {
+  final String krama;
+  final String meaning;
+  final String element;
+  final String color;
+  final String direction;
+  final String posture;
+
+  AssociatedSymbols({
+    this.krama,
+    this.meaning,
+    this.element,
+    this.color,
+    this.direction,
+    this.posture,
+  });
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      "krama": this.krama ?? "",
+      "meaning": this.meaning ?? "",
+      "element": this.element ?? "",
+      "color": this.color ?? "",
+      "direction": this.direction ?? "",
+      "posture": this.posture ?? "",
+    };
   }
 }
--- test/javanese_date/pasaran_test.dart
+++ test/javanese_date/pasaran_test.dart
@@ -5,13 +5,33 @@ import 'package:javanese_date_converter/javanese_date/pasaran.dart';
 void main() {
   group('Should return a valid pasaran', () {
     test('Should return a correct pasaran type', () {
-      expect(Pasaran(JavaneseDate(day: 21, month: 4, year: 1879)).pasaranType, PasaranType.PAHING);
-      expect(Pasaran(JavaneseDate(day: 17, month: 9, year: 1904)).pasaranType, PasaranType.PAHING);
-      expect(Pasaran(JavaneseDate(day: 8, month: 7, year: 1633)).pasaranType, PasaranType.LEGI);
+      expect(Pasaran(JavaneseDate(day: 21, month: 4, year: 1879)).pasaranType,
+          PasaranType.PAHING);
+      expect(Pasaran(JavaneseDate(day: 17, month: 9, year: 1904)).pasaranType,
+          PasaranType.PAHING);
+      expect(Pasaran(JavaneseDate(day: 8, month: 7, year: 1633)).pasaranType,
+          PasaranType.LEGI);
     });
     test('Should return a correct pasaran', () {
-      expect(Pasaran(JavaneseDate(day: 21, month: 4, year: 1879)).pasaran, "Pahing");
-      expect(Pasaran(JavaneseDate(day: 8, month: 7, year: 1633)).pasaran, "Lêgi");
+      expect(Pasaran(JavaneseDate(day: 21, month: 4, year: 1879)).pasaran,
+          "Pahing");
+      expect(
+          Pasaran(JavaneseDate(day: 8, month: 7, year: 1633)).pasaran, "Lêgi");
+    });
+
+    test('Should return a correct associated symbols', () {
+      expect(
+          Pasaran(
+            JavaneseDate(day: 21, month: 4, year: 1879),
+          ).associatedSymbols,
+          {
+            "krama": "Pahit",
+            "meaning": "bitter",
+            "element": "fire",
+            "color": "red",
+            "direction": "south",
+            "posture": "to face",
+          });
     });
   });
 }
